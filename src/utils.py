d376ce2369e6195635198f4c37de00e86058543e
Paul Duan <duanxianzhao@gmail.com>
2025-11-09 17:59:00 -0500

    keywords baseline experiments

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,11 @@
 from src.dataset import load_dataset
 
+_, _, mapping, _ = load_dataset()
 def label_to_emoji(label):
-    _, _, mapping, _ = load_dataset()
 
     label_emoji_mapping = dict(zip(mapping['number'], mapping['emoticons']))
     
     return label_emoji_mapping.get(label, "❓")
+
+def labels_to_emojis(labels):
+    return [label_to_emoji(label) for label in labels]
